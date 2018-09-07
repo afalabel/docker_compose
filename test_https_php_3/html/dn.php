f83f93b9ed421608f5f50b3b1261f31926412da6
Antonio Falabella <antonio.falabella@cnaf.infn.it>
2018-09-07 12:37:23 +0200

    test 3

---
+++ test_https_php_3/html/dn.php
@@ -0,0 +1,18 @@
+<?php
+echo '<pre>';
+echo '<tr><td>'.'VOMS_USER is '.'</td><td>' . $_SERVER['VOMS_USER'] ."\n". '</td></tr>' ;
+echo '<tr><td>'.'EE_S_DN is '.'</td><td>' . $_SERVER['EE_S_DN'] ."\n". '</td></tr>' ;
+echo '<tr><td>'.'VOMS_USER_CA is '.'</td><td>' . $_SERVER['VOMS_USER_CA'] ."\n". '</td></tr>' ;
+echo '<tr><td>'.'EE_I_DN is '.'</td><td>' . $_SERVER['EE_I_DN'] ."\n". '</td></tr>' ;
+echo '<tr><td>'.'VOMS_FQANS is '.'</td><td>' . $_SERVER['VOMS_FQANS'] ."\n". '</td></tr>' ;
+echo '<tr><td>'.'VOMS_SERVER is '.'</td><td>' . $_SERVER['VOMS_SERVER'] ."\n". '</td></tr>' ;
+echo '<tr><td>'.'VOMS_SERVER_CA is '.'</td><td>' . $_SERVER['VOMS_SERVER_CA'] ."\n". '</td></tr>' ;
+echo '<tr><td>'.'VOMS_VO is '.'</td><td>' . $_SERVER['VOMS_VO'] ."\n". '</td></tr>' ;
+echo '<tr><td>'.'VOMS_SERVER_URI is '.'</td><td>' . $_SERVER['VOMS_SERVER_URI'] ."\n". '</td></tr>' ;
+echo '<tr><td>'.'VOMS_NOT_BEFORE is '.'</td><td>' . $_SERVER['VOMS_NOT_BEFORE'] ."\n". '</td></tr>' ;
+echo '<tr><td>'.'VOMS_NOT_AFTER is '.'</td><td>' . $_SERVER['VOMS_NOT_AFTER'] ."\n". '</td></tr>' ;
+echo '<tr><td>'.'VOMS_GENERIC_ATTRIBUTES is '.'</td><td>' . $_SERVER['VOMS_GENERIC_ATTRIBUTES'] ."\n". '</td></tr>' ;
+echo '<tr><td>'.'VOMS_NOT_BEFORE is '.'</td><td>' . $_SERVER['VOMS_GENERIC_ATTRIBUTES'] ."\n". '</td></tr>' ;
+echo '<tr><td>'.'VOMS_SERIAL is '.'</td><td>' . $_SERVER['VOMS_SERIAL'] ."\n". '</td></tr>' ;
+echo '</pre>';
+?>
---
+++ test_https_php_3/html/test.php
@@ -0,0 +1,6 @@
+<?php
+echo '<pre>';
+echo "\n".'$SCRIPT_NAME'."\n"; var_dump($_SERVER);
+echo '<tr><td>'.'DN'.'</td><td>' . $_SERVER['DN'] . '</td></tr>' ; 
+echo '</pre>';
+?>
